43ce975bc9a5f94f0d460e40521f164465bb69a8
Kevin Lam <kevin127lam@gmail.com>
2024-04-08 11:53:16 -0400

    added script to read text file using promises

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 
 const express = require("express");
 const path = require("path");
+// Include fs module
+const fs = require('fs');
+const fsPromises = require('fs').promises;
 
 const app = express();
 
@@ -9,6 +12,31 @@ app.use(express.static(
   path.resolve(__dirname, "public")
 ));
 
-app.listen(3000, () => console.log("Starting up Top 40 Search"));
+let words = [];
+
+// read in a text file
+async function readFile() {
+  try {
+    const data = await fsPromises.readFile('textfile.txt', 'utf8');
+    words = data.split(' ').map(word => word.trim());
+    console.log('Words loaded successfully.');
+  } catch (err) {
+    console.error('Error reading file:', err);
+  }
+}
+
+readFile();
+
+
+// router to return a word to client
+app.get('/random', (req, res) => {
+  const randIndex = Math.floor(Math.random() * words.length);
+  const randWord = words[randIndex];
+  
+  //sends raondom word to client
+  res.send(randWord);
+});
+
+app.listen(3000, () => console.log("Starting up Crazy Words"));
 
 
